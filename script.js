d755012c5b0e0165c62e35fd25e529b24666f483
danialakbar376-ui <danialakbar376@gmail.com>
2026-06-30 20:45:07 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,88 @@
+function openGift(){
+document.getElementById("giftSection")
+.style.display="block";
+
+window.scrollTo({
+top:window.innerHeight,
+behavior:"smooth"
+});
+}
+
+const images=[
+"images/deandra1.jpg",
+"images/deandra2.jpg",
+"images/deandra3.jpg",
+"images/deandra4.jpg",
+"images/deandra5.jpg",
+"images/deandra6.jpg"
+];
+
+const quotes=[
+"Every picture of you feels like my favorite memory.",
+"You are beautiful in every universe.",
+"My favorite view has always been you.",
+"Your smile is my favorite place.",
+"Every day with you is a blessing.",
+"My heart chose you long ago."
+];
+
+let current=0;
+
+setInterval(()=>{
+
+current++;
+
+if(current>=images.length){
+current=0;
+}
+
+document.getElementById("slide").src=
+images[current];
+
+document.getElementById("quote").innerText=
+quotes[current];
+
+},3000);
+
+function createHeart(){
+
+const heart=document.createElement("div");
+
+heart.innerHTML="🤍";
+
+heart.style.position="absolute";
+heart.style.left=Math.random()*100+"vw";
+heart.style.top="-20px";
+heart.style.fontSize=
+(Math.random()*20+20)+"px";
+
+heart.style.animation=
+"fall 8s linear";
+
+document.getElementById("hearts")
+.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},8000);
+
+}
+
+setInterval(createHeart,500);
+
+const style=document.createElement("style");
+
+style.innerHTML=`
+@keyframes fall{
+0%{
+transform:translateY(0);
+opacity:1;
+}
+100%{
+transform:translateY(100vh);
+opacity:0;
+}
+}
+`;
+
+document.head.appendChild(style);
